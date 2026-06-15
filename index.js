2b691acc9e909390bef404fa23c2ee2b248b6080
Stella0711 <crystal.quigley@gmail.com>
2026-06-15 09:23:25 -0500

    started java script to access api, ran some tests

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-const userListEl = document.querySelector(".user-list");
+// const userListEl = document.querySelector(".user-list");
 
-async function main() {
-  const users = await fetch("https://jsonplaceholder.typicode.com/users");
-  const usersData = await users.json();
+// async function main() {
+//   const users = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const usersData = await users.json();
   
-  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
-}
+//   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+// }
 
-  main();
+//   main();
 
-  function showUserPosts(id) {
-    localStorage.setItem("id", id);
-    window.location.href = `${window.location.origin}/user.html`
-}
+//   function showUserPosts(id) {
+//     localStorage.setItem("id", id);
+//     window.location.href = `${window.location.origin}/user.html`
+// }
     
 
-  function userHTML(user) {
-       return `<div class="user-card" onclick ="showUserPosts(${user.id})">
-            <div class="user-card__container">
-            <h3>${user.name}</h3>
-                <p><b>Email:</b> ${user.email}</p>
-                <p><b>Phone:</b> ${user.phone}</p>
-                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
-                ${user.website}</a></p>
-            </div>
-         </div>`;
-  }
+//   function userHTML(user) {
+//        return `<div class="user-card" onclick ="showUserPosts(${user.id})">
+//             <div class="user-card__container">
+//             <h3>${user.name}</h3>
+//                 <p><b>Email:</b> ${user.email}</p>
+//                 <p><b>Phone:</b> ${user.phone}</p>
+//                 <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+//                 ${user.website}</a></p>
+//             </div>
+//          </div>`;
+//   }
 
-  console.log("Hello World!");
+const input = document.querySelector("#movieInput");
+const button = document.querySelector("#searchBtn");
+const movieContainer = document.querySelector("#results .results__grid");
     
+button.addEventListener("click", async () => {
+    const movieTitle = input.value;
+
+    console.log(movieTitle);
+
+    const response = await fetch(
+        `http://www.omdbapi.com/?i=tt3896198&apikey=919dd8b6&s=${movieTitle}`
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+});
+
+// this is a test. it works! however, the syntax or formatting is slightly different than what David taught...i think.
+
 
